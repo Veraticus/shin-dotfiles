2a62d484ee906599c7c6086588ea004192ca9cf7
Josh Symonds <veraticus@gmail.com>
2019-09-29 10:47:36 -0500

    Remove timestamp of git

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,1 +1,20 @@
-GEOMETRY_RPROMPT=(geometry_exec_time geometry_git geometry_echo}
+josh_geometry_git() {
+  (( $+commands[git] )) || return
+
+  command git rev-parse --git-dir > /dev/null 2>&1 || return
+
+  $(command git rev-parse --is-bare-repository 2>/dev/null) \
+    && ansi ${GEOMETRY_GIT_COLOR_BARE:=blue} ${GEOMETRY_GIT_SYMBOL_BARE:="⬢"} \
+    && return
+
+  local geometry_git_details && geometry_git_details=(
+    $(geometry_git_conflicts)
+    $(geometry_git_stashes)
+    $(geometry_git_status)
+  )
+
+  local separator=${GEOMETRY_GIT_SEPARATOR:-" :: "}
+  echo -n $(geometry_git_symbol) $(geometry_git_branch) ${(pj.$separator.)geometry_git_details}
+}
+
+GEOMETRY_RPROMPT=(geometry_exec_time josh_geometry_git geometry_echo)
