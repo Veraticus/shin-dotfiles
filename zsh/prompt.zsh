997d24e9548715737255a8ef0993349aed9380f3
Josh Symonds <veraticus@gmail.com>
2019-09-29 10:50:17 -0500

    Improve geometry

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -17,4 +17,7 @@ josh_geometry_git() {
   echo -n $(geometry_git_symbol) $(geometry_git_branch) ${(pj.$separator.)geometry_git_details}
 }
 
+GEOMETRY_STATUS_SYMBOL="▶"
+GEOMETRY_STATUS_SYMBOL_ERROR="▷"
+GEOMETRY_PROMPT=(geometry_echo geometry_path geometry_status)
 GEOMETRY_RPROMPT=(geometry_exec_time josh_geometry_git geometry_echo)
