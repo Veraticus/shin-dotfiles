1fab13bddced142fa8b1bc646dbd3a7b98de61d9
Josh Symonds <veraticus@gmail.com>
2021-10-29 15:58:19 -0700

    Fix formatting

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -69,6 +69,20 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] =
         underline = true
     })
 
+vim.lsp.handlers["textDocument/formatting"] = function(err, _, result, _, bufnr)
+    if err ~= nil or result == nil then
+        return
+    end
+    if not vim.api.nvim_buf_get_option(bufnr, "modified") then
+        local view = vim.fn.winsaveview()
+        vim.lsp.util.apply_text_edits(result, bufnr)
+        vim.fn.winrestview(view)
+        if bufnr == vim.api.nvim_get_current_buf() then
+            vim.api.nvim_command("noautocmd :update")
+        end
+    end
+end
+
 local function make_on_attach(config)
     return function(client)
         if config.before then config.before(client) end
